9a1db59eab7c5e94e4c5bb68b55291df1c13ae85
Ángel Luis <45455525+angelui@users.noreply.github.com>
2018-12-10 01:18:23 +0100

    Reserve correct

--- script/reserve.js
+++ script/reserve.js
@@ -205,7 +205,7 @@ function redirect(where){ // Redirect
 }
 
 function pay(){ // Reserve button
-    if(JSON.parse(localStorage.getItem('globalVariables')).logged != 0 && document.getElementById('arriveDate2').value != "" && document.getElementById('leftDate2').value != ""){
+    if(JSON.parse(localStorage.getItem('globalVariables')).logged == 0 && document.getElementById('arriveDate2').value != "" && document.getElementById('leftDate2').value != ""){
         redirect('pay.html');
     }
     else{
